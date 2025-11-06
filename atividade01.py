27a79bdba7774de80866ab82e032d08abfb0cf4a
Juan <henzojuan82@gmail.com>
2025-11-06 17:03:45 -0300

    exercicios

--- atividade01.py
+++ atividade01.py
@@ -1,1 +1,8 @@
+nome = input("Qual é o seu nome? ")
+idade_texto = input("Qual é a sua idade? ")
+idade_numero = int(idade_texto)
 
+if idade_numero >= 18:
+    print(f"Olá, {nome}, você é maior de idade.")
+else:
+    print(f"Olá, {nome}, você é menor de idade.")
--- atividade02.py
+++ atividade02.py
@@ -1,1 +1,7 @@
+numero_str = input("Digite um número para ver a tabuada: ")
+numero = int(numero_str)
 
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
