5cae0fe818ee941497a612402bd42ff71e732f30
Juan <henzojuan82@gmail.com>
2025-11-08 13:06:22 -0300

    exercicio 01 da repo de 8/11

--- funcao_calculadora.py
+++ funcao_calculadora.py
@@ -1,1 +1,19 @@
+def calculadora(numero1, numero2, operacao):
+    if operacao == '+':
+        return numero1 + numero2
+    elif operacao == '-':
+        return numero1 - numero2
+    elif operacao == '*':
+        return numero1 * numero2
+    elif operacao == '/':
+        return numero1 / numero2
+    else:
+        return "Operação inválida"
 
+
+# teste ;-;
+
+print(calculadora(10, 5, '+'))
+print(calculadora(8, 2, '/'))
+print(calculadora(100, 5, '*'))
+print(calculadora(50, 10, '-'))
