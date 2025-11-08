235167ba7bad0a132ebcbd4e435be94a0b0e1626
Juan <henzojuan82@gmail.com>
2025-11-08 13:24:32 -0300

    exercício 02 da reposição de 8/11

--- heranca_veiculos.py
+++ heranca_veiculos.py
@@ -1,1 +1,16 @@
+# classe mãe
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+
+# classe filha
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, cor, numero_portas):
+        super().__init__(marca, modelo, cor)  # herdando os atributos da classe base
+        self.numero_portas = numero_portas
+
+
+
 
